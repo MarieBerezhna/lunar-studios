953344598471e64124215f6b9bd8184dd1bd6f3d
marie <marie.berezhna@gmail.com>
2022-03-07 15:09:16 +0200

    server middleware

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,7 +38,5 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   },
-  serverMiddleware: [
-          { path: '/api', handler: '~/api/index' },
-        ],
+  serverMiddleware: ['~/api'],
 }
