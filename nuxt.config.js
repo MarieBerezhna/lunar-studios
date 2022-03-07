3e3049e7e2717913b8b6399aa0fd42d686131445
marie <marie.berezhna@gmail.com>
2022-03-08 01:26:27 +0200

    add axios config

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,18 +34,7 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: ['@nuxtjs/axios'],
   axios: {
-    baseURL: 'http://localhost:3000', // Used as fallback if no runtime config is provided
-  },
-  publicRuntimeConfig: {
-    axios: {
-      browserBaseURL: process.env.BROWSER_BASE_URL
-    }
-  },
-
-  privateRuntimeConfig: {
-    axios: {
-      baseURL: process.env.BASE_URL
-    }
+    baseURL: process.env.BASE_URL || 'http://localhost:3000', // Used as fallback if no runtime config is provided
   },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
