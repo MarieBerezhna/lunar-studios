ed6b3b4e2850064991b690f480ffb098571346ee
marie <marie.berezhna@gmail.com>
2022-03-07 15:13:31 +0200

    server middleware

--- api/index.js
+++ api/index.js
@@ -4,10 +4,10 @@ import bodyParser from 'body-parser'
 const app = express()
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.status(200).json('test - get').end()
 })
-app.post('/', (req, res) => {
+app.post('/api', (req, res) => {
   res.status(200).json('test - post').end()
 })
 
