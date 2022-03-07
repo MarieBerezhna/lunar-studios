53cbec50cdd84adddbbf4bd35e4a1d6901b1831a
marie <marie.berezhna@gmail.com>
2022-03-07 18:43:00 +0200

    axios base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: ['@nuxtjs/axios'],
   axios: {
-    baseURL: process.env.BASE_URL, // Used as fallback if no runtime config is provided
+    baseURL: 'https://lunar-studios.vercel.app', // Used as fallback if no runtime config is provided
   },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
