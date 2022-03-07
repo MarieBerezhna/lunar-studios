c632a75b25ee98de5e958c03e6764f0bb4dcaafd
marie <marie.berezhna@gmail.com>
2022-03-08 01:20:10 +0200

    add axios config

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,20 +33,20 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: ['@nuxtjs/axios'],
-  // axios: {
-  //   baseURL: 'https://lunar-studios.vercel.app', // Used as fallback if no runtime config is provided
-  // },
-  // publicRuntimeConfig: {
-  //   axios: {
-  //     browserBaseURL: process.env.BROWSER_BASE_URL
-  //   }
-  // },
+  axios: {
+    baseURL: 'http://localhost:3000', // Used as fallback if no runtime config is provided
+  },
+  publicRuntimeConfig: {
+    axios: {
+      browserBaseURL: process.env.BROWSER_BASE_URL
+    }
+  },
 
-  // privateRuntimeConfig: {
-  //   axios: {
-  //     baseURL: process.env.BASE_URL
-  //   }
-  // },
+  privateRuntimeConfig: {
+    axios: {
+      baseURL: process.env.BASE_URL
+    }
+  },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   },
