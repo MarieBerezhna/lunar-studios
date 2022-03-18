492a5b17572a6e937b7b3f1edee5204f42f10b53
marie <marie.berezhna@gmail.com>
2022-03-18 14:48:18 +0200

    logo + bootstrap

--- components/shared/TopNav.vue
+++ components/shared/TopNav.vue
@@ -1,13 +1,29 @@
 <template>
     <div>
-        <ul class="bg-success">
-            <li>Home</li>
-            <li>About</li>
-            <li>Works</li>
-            <li>Collaborations</li>
-            <li>Contact</li>
-            <li></li>
-        </ul>
+  <b-navbar toggleable="lg" type="light" variant="white">
+    <b-navbar-brand href="/">
+        <img width="77" height="100" :src="require('../../static/assets/logo.png')" alt="">
+    </b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav right>
+      <b-navbar-nav>
+        <b-nav-item href="#">Link</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <!-- <b-nav-item-dropdown right>
+          <template #button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown> -->
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
     </div>
 </template>
 
--- plugins/bootstrap.js
+++ plugins/bootstrap.js
@@ -2,3 +2,5 @@ import Vue from 'vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
